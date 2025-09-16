70d60cbcc4f3c62fd14c540b2e063f28eca8e0de
mudasirhassandar <mudasirhassan391@gmail.com>
2025-09-16 11:46:28 +0530

    Update directCall.c

--- directCall.c
+++ directCall.c
@@ -3,7 +3,11 @@
 int fact(int n)
 {
     if (n == 1 || n == 0)
+    {
+        printf("fact(%d)=1\n", n);
         return 1;
+    }
+    printf("%d*fact(%d)\n", n, n - 1);
     return n * fact(n - 1);
 }
 int main()
@@ -11,13 +15,14 @@ int main()
     int n;
     printf("Enter the number = ");
     scanf("%d", &n);
-    printf("factorial of 5 :-\n");
-    for (int i = n; i >= 1; i--)
-    {
-        printf("%d ", i);
-        if (i != 1)
-            printf("* ");
-    }
+    // printf("factorial of 5 :-\n");
+    // for (int i = n; i >= 1; i--)
+    // {
+    //     printf("%d ", i);
+    //     if (i != 1)
+    //         printf("* ");
+    // }
+    // printf("\n");
     int factorial = fact(n);
     printf("= %d", factorial);
 
