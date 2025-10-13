2430af7aaa05678325598de81a9e14ad255698c8
Mudasir Hassan <mudasirhassan391@gmail.com>
2025-10-13 22:31:40 +0530

    Add files via upload

---
+++ traversal_OF_circular_LL.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;          // data part
+    struct node *next; // stores the address of next node.
+};
+void traversal(struct node *head)
+{
+
+    struct node *current = head;
+    if (current == NULL)
+    {
+        printf("Linked List is Empty\n");
+        return;
+    }
+    // printing the linked list.
+    printf(" Circular Linked list:\n");
+    do
+    {
+        printf("%d->", current->data);
+        current = current->next;
+    } while (current != head);
+    printf("(Back to head)");
+
+    return;
+}
+int main()
+{
+    // ceration of nodes.
+    struct node *head = NULL;
+    struct node *second = NULL;
+    struct node *third = NULL;
+    // allocating memory for node in heap.
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    // assign data.
+    head->data = 10;
+    second->data = 20;
+    third->data = 30;
+    // connecting nodes.
+    head->next = second;
+    second->next = third;
+    third->next = head;
+    traversal(head);
+    return 0;
+}
